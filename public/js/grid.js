3ad0f5dec9f0b436b3caff00479af750c2d76eb9
Chris Troutner <chris.troutner@gmail.com>
2018-05-09 19:37:30 -0700

    Beginning to rebuild listings

--- public/js/grid.js
+++ public/js/grid.js
@@ -1,73 +1,77 @@
-function generateGrid (deviceModels, obContracts) {
+function generateGrid(deviceModels, obContracts) {
   // debugger;
 
   // register the grid component
-  Vue.component('demo-grid', {
-    template: '#grid-template',
+  Vue.component("demo-grid", {
+    template: "#grid-template",
     replace: true,
     props: {
       data: Array,
       columns: Array,
       filterKey: String
     },
-    data: function () {
-      var sortOrders = {}
-      this.columns.forEach(function (key) {
-        sortOrders[key] = 1
-      })
+    data: function() {
+      var sortOrders = {};
+      this.columns.forEach(function(key) {
+        sortOrders[key] = 1;
+      });
       return {
-        sortKey: '',
+        sortKey: "",
         sortOrders: sortOrders
-      }
+      };
     },
     computed: {
-      filteredData: function () {
-        var sortKey = this.sortKey
-        var filterKey = this.filterKey && this.filterKey.toLowerCase()
-        var order = this.sortOrders[sortKey] || 1
-        var data = this.data
+      filteredData: function() {
+        var sortKey = this.sortKey;
+        var filterKey = this.filterKey && this.filterKey.toLowerCase();
+        var order = this.sortOrders[sortKey] || 1;
+        var data = this.data;
         if (filterKey) {
-          data = data.filter(function (row) {
-            return Object.keys(row).some(function (key) {
-              return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-            })
-          })
+          data = data.filter(function(row) {
+            return Object.keys(row).some(function(key) {
+              return (
+                String(row[key])
+                  .toLowerCase()
+                  .indexOf(filterKey) > -1
+              );
+            });
+          });
         }
         if (sortKey) {
-          data = data.slice().sort(function (a, b) {
-            a = a[sortKey]
-            b = b[sortKey]
-            return (a === b ? 0 : a > b ? 1 : -1) * order
-          })
+          data = data.slice().sort(function(a, b) {
+            a = a[sortKey];
+            b = b[sortKey];
+            return (a === b ? 0 : a > b ? 1 : -1) * order;
+          });
         }
 
         // if(data.description === "<b>Comming Soon</b>") console.log('Mark');
         // console.log(JSON.stringify(data,null,2));
 
-        return data
+        return data;
       }
-
     },
     filters: {
-      capitalize: function (str) {
-        return str.charAt(0).toUpperCase() + str.slice(1)
+      capitalize: function(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
       }
     },
     methods: {
-      sortBy: function (key) {
-        this.sortKey = key
-        this.sortOrders[key] = this.sortOrders[key] * -1
+      sortBy: function(key) {
+        this.sortKey = key;
+        this.sortOrders[key] = this.sortOrders[key] * -1;
       },
 
-      convertToHTML: function (input) {
-        var storeUrl = 'ob://QmYfMAnHQrHAKrWD6bENvD7Z4JCgvdxFJgKzt2kCpCTidw/store/';
-        var slug = '';
+      convertToHTML: function(input) {
+        var storeUrl =
+          "ob://QmPUf97TtPT1JapY6ey1tc9tiErcUpZPoTQTx5KVD2rQ5r/store";
+        var slug = "";
 
-        if (input.toString().indexOf('ob:') > -1) {
+        if (input.toString().indexOf("ob:") > -1) {
           slug = input.slice(3, input.length);
           // input = `<a type="button" class="btn btn-xs btn-primary" href=${storeUrl+slug}>See Listing</a>`;
           input = `<button type='button' class='btn btn-primary btn-xs' onclick="openModal(1,'${slug}')">See Listing</button>`;
-        } else if (input.toString().indexOf('desc:') > -1) {
+        } else if (input.toString().indexOf("desc:") > -1) {
           desc = input.slice(5, input.length);
           input = `<button type='button' class='btn btn-primary btn-xs' onclick="openModal(2,'${desc}')">See Info</button>`;
         }
@@ -77,12 +81,14 @@ function generateGrid (deviceModels, obContracts) {
         return input;
       }
     }
-  })
+  });
 
   var listingData = [];
   for (var i = 0; i < obContracts.length; i++) {
     var thisContract = obContracts[i];
-    var thisDeviceModel = deviceModels.find(model => model.obContract === obContracts[i]._id);
+    var thisDeviceModel = deviceModels.find(
+      model => model.obContract === obContracts[i]._id
+    );
 
     // Continue if the device doesn't have an OB contract.
     if (thisDeviceModel === undefined) continue;
@@ -90,21 +96,29 @@ function generateGrid (deviceModels, obContracts) {
     var thisListing = {};
     thisListing.price = formatPrice(thisContract.price);
     thisListing.name = thisDeviceModel.deviceName;
-    thisListing.uptime = 'N/A';
-    thisListing.memory = 'N/A';
-    thisListing.diskspace = 'N/A';
-    thisListing.listing = 'ob:' + thisContract.listingSlug;
-    thisListing.description = 'desc:' + thisDeviceModel._id;
+    thisListing.uptime = "N/A";
+    thisListing.memory = "N/A";
+    thisListing.diskspace = "N/A";
+    thisListing.listing = "ob:" + thisContract.listingSlug;
+    thisListing.description = "desc:" + thisDeviceModel._id;
 
     listingData.push(thisListing);
   }
 
   // bootstrap the demo
   var demo = new Vue({
-    el: '#demo',
+    el: "#demo",
     data: {
-      searchQuery: '',
-      gridColumns: ['price', 'name', 'uptime', 'memory', 'diskspace', 'listing', 'description'],
+      searchQuery: "",
+      gridColumns: [
+        "price",
+        "name",
+        "uptime",
+        "memory",
+        "diskspace",
+        "listing",
+        "description"
+      ],
       // gridData: [
       //  { price: 2.51, name: 'Chuck Norris', uptime: Infinity, memory: '1', diskspace: 54, description: 'desc' },
       //  { price: 2.52, name: 'Bruce Lee', uptime: 9000, memory: '2', diskspace: 45, description: 'desc' },
@@ -114,13 +128,13 @@ function generateGrid (deviceModels, obContracts) {
       gridData: listingData
       // Price = rent button, deviceName, Uptime, Memory, Disk space, description button,
     }
-  })
+  });
 }
 
-function formatPrice (price) {
-  var thisPrice = Number(price)
+function formatPrice(price) {
+  var thisPrice = Number(price);
   thisPrice = (thisPrice / 100).toFixed(2);
-  thisPrice = '$' + thisPrice + ' per Hour';
+  thisPrice = "$" + thisPrice + " per Hour";
 
   return thisPrice;
 }
--- public/js/p2pvps.js
+++ public/js/p2pvps.js
@@ -1,33 +1,31 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
   var devicePublicModels;
   var obContractModels;
 
   // get the devicePublicModels for each listed device.
   getDeviceData()
+    .then(data => {
+      devicePublicModels = data;
 
-  .then(data => {
-    devicePublicModels = data;
+      // console.log(`devicePublicModels = ${JSON.stringify(data,null,2)}`);
 
-    // console.log(`devicePublicModels = ${JSON.stringify(data,null,2)}`);
+      return getObContractData();
+    })
 
-    return getObContractData();
-  })
+    // get the obContract model for each listed device.
+    .then(data => {
+      obContractModels = data;
 
-  // get the obContract model for each listed device.
-  .then(data => {
-    obContractModels = data;
+      console.log(`obContractModels = ${JSON.stringify(data, null, 2)}`);
 
-    console.log(`obContractModels = ${JSON.stringify(data, null, 2)}`);
-
-    return generateGrid(devicePublicModels, obContractModels);
-  })
+      return generateGrid(devicePublicModels, obContractModels);
+    });
 });
 
 // Retrieve all devicePublicModels from the server.
-function getDeviceData () {
-  return new Promise(function (resolve, reject) {
-    return $.get('/api/devicePublicData/list', '', function (data) {
+function getDeviceData() {
+  return new Promise(function(resolve, reject) {
+    return $.get("/api/devicePublicData/list", "", function(data) {
       // debugger;
 
       var collection = data.collection;
@@ -38,9 +36,9 @@ function getDeviceData () {
 }
 
 // Retrieve all obContract models from the server.
-function getObContractData () {
-  return new Promise(function (resolve, reject) {
-    return $.get('/api/obContract/list', '', function (data) {
+function getObContractData() {
+  return new Promise(function(resolve, reject) {
+    return $.get("/api/obContract/list", "", function(data) {
       // debugger;
 
       var collection = data.collection;
@@ -51,56 +49,53 @@ function getObContractData () {
 }
 
 // Open the modal
-function openModal (type, arg) {
-  var modal = $('#myModal')
+function openModal(type, arg) {
+  var modal = $("#myModal");
 
   // Show a listing
   if (type === 1) {
-    var storeUrl = 'ob://QmYfMAnHQrHAKrWD6bENvD7Z4JCgvdxFJgKzt2kCpCTidw/store/';
-    var slug = '';
+    var storeUrl = "ob://QmPUf97TtPT1JapY6ey1tc9tiErcUpZPoTQTx5KVD2rQ5r/store";
+    var slug = "";
 
     var bodyMsg = `
 <p>Click the button below and your browser open the listing in your OpenBazaar client.</p>
-<center><a type="button" class="btn btn-primary" href="${storeUrl + arg}">Open OpenBazaar</a></center>
+<center><a type="button" class="btn btn-primary" href="${storeUrl +
+      arg}">Open OpenBazaar</a></center>
 <br><br>
 <p></p>
 <p>If the button does not take you to the listing, paste the below URL in your OpenBazaar client:</p>
 <center><p>${storeUrl + arg}</p></center>
     `;
 
-    modal.find('.modal-title').text('View Listing');
+    modal.find(".modal-title").text("View Listing");
 
-    modal.find('.modal-body').empty();
-    modal.find('.modal-body').append(bodyMsg);
+    modal.find(".modal-body").empty();
+    modal.find(".modal-body").append(bodyMsg);
 
-    modal.modal('show');
+    modal.modal("show");
   }
 
   // Show the description associated with the selected listing.
   else {
-    modal.find('.modal-title').text('Listing Description');
+    modal.find(".modal-title").text("Listing Description");
 
-    modal.find('.modal-body').empty();
+    modal.find(".modal-body").empty();
 
-    getDescription(arg)
+    getDescription(arg).then(desc => {
+      modal.find(".modal-body").append(`<p>${desc}</p>`);
 
-    .then(desc => {
-      modal.find('.modal-body').append(`<p>${desc}</p>`);
-
-      modal.modal('show');
+      modal.modal("show");
     });
   }
 }
 
 // Retrieve the description for the selected listing.
-function getDescription (id) {
-  return getDeviceData()
-
-  .then(deviceModels => {
+function getDescription(id) {
+  return getDeviceData().then(deviceModels => {
     // debugger;
 
     var thisDeviceModel = deviceModels.find(model => model._id === id);
 
     return thisDeviceModel.deviceDesc;
-  })
+  });
 }
