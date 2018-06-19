1c41b44e9b0694bb99fec41c91f6f3b7b56208bd
Chris Troutner <chris.troutner@gmail.com>
2018-06-18 17:52:10 -0700

    Pointing API calls to localhost intead of p2pvps.net

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: {
-      '/api': {
-        target: `http://p2pvps.net:3001`,
+      "/api": {
+        //target: `http://p2pvps.net:3001`,
+        target: `http://localhost:3001`,
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '/api'
+          "^/api": "/api"
         }
       }
     }
