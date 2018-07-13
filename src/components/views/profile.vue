02736ddb653ff3b5f2334786e4762b8e33cb3252
Chris Troutner <chris.troutner@gmail.com>
2018-07-13 04:44:25 +0000

    Editing of address is in place. Need to update server

--- src/components/views/profile.vue
+++ src/components/views/profile.vue
@@ -15,9 +15,14 @@
       <div class="row">
         <div class="col-xs-12 text-center">
           <p>BCH Address</p>
-          <div v-on:click="editFlag=!editFlag">
-            <p v-if="editFlag"><small>{{ this.$store.state.userInfo.bchAddress }}</small></p>
-            <p v-if="!editFlag">test</p>
+          <div >
+            <p v-if="editFlag" v-on:click="editFlag=!editFlag"><small>{{ bchAddress }}</small></p>
+
+            <form v-if="!editFlag" class="navbar-form">
+              <div class="form-group">
+                <input type="text" class="form-control" placehold="Add your BCH address" v-on:keyup.enter="updateAddress" v-model="bchAddress">
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -42,19 +47,26 @@ export default {
   data() {
     return {
       msg: 'This is the profile view',
-      editFlag: false
+      editFlag: false,
+      bchAddress: this.$store.state.userInfo.bchAddress
     }
   },
 
   methods: {
-    logOut: function () {
-      $.post('/keystone/api/session/signout', '', function (data) {
-        if (!data.success) {
-          console.error('Something went wrong with trying to log out. See App.vue logOut() method.')
-        }
-      })
+    updateAddress: function() {
+
+      // Update the bchAddress for the user on the server.
+      persistAddressChange();
+
+      // Toggle the form off and display the value.
+      this.editFlag=!this.editFlag
     }
   },
+}
+
+// Updates the user model on the server, as well as the Vuex store.
+function persistAddressChange() {
+  
 }
 
 </script>
